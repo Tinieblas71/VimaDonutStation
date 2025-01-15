d07648983fb9e10572e54560d21256505143c2a5
Tinieblas71 <morenoestradasantiago@gmail.com>
2025-01-14 21:34:12 -0600

    Correcciones y funcion añadida para complemetar esta pagina web

--- js/script.js
+++ js/script.js
@@ -44,3 +44,20 @@ document.querySelector('.home').onmouseleave = () =>{
 function changeImg(anything){
     document.querySelector('.home-img').src = anything;
 }
+
+
+const send = document.querySelector('#send-order');
+send.addEventListener('click', (e) => {
+    e.preventDefault();
+    const name = document.querySelector('#name').value;
+    const phone = document.querySelector('#order').value;
+    const order = document.querySelector('#order').value;
+    const howMuch = document.querySelector('#how-much').value;
+    const details = document.querySelector('#details').value;
+    const date = document.querySelector('#date').value;
+    const address = document.querySelector('#address').value;
+
+    const link = `https://api.whatsapp.com/send?phone=5215612807356&text=Confirmaci%C3%B3n%20de%20Pedido%3A%20Vima%20Donut%20Station%0ANombre%3A%20${name}%0ANumero%3A%20${phone}%0AOrden%3A%20${order}%20(${howMuch})%0AFecha%3A%20${date}%0ADirecci%C3%B3n%3A%20${address}%0ADetalles%3A%20${details}%0AEn%20unos%20momentos%20atenderemos%20su%20pedido.`;
+    window.open(link);
+
+})
